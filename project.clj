8f0999347f181d3c2f5c37ca920e6dc2621db2af
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2020-12-04 10:13:43 +0100

    wip

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/kwrooijen/advent_of_code_2020"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies []
+  :dependencies [[clj-http "3.10.3"]]
   :plugins [[lein-cloverage "1.0.13"]
             [lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]
@@ -11,6 +11,7 @@
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
   :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[aoc_2020 \"[0-9.]*\"\\\\]/[aoc_2020 \"${:version}\"]/" "README.md"]}
+  :resource-paths ["resources" "local"]
   :release-tasks [["shell" "git" "diff" "--exit-code"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
--- src/aoc_2020/util.clj
+++ src/aoc_2020/util.clj
@@ -1,8 +1,22 @@
 (ns aoc-2020.util
   (:require
+   [clj-http.client :as http]
    [clojure.java.io :as io]))
 
+(defn from-http [i]
+  (if-let [session-resource (io/resource "session")]
+    (-> "https://adventofcode.com/2020/day/"
+        (str i "/input")
+        (http/get {:cookies {"session" {:value (slurp session-resource)}}})
+        :body)
+    (throw (ex-data "No session file found in local/session"))))
+
+(defn resource-file [i]
+  (str (System/getProperty "user.dir") "/resources/input/" i))
+
 (defn input [i]
-  (-> (str "input/" i)
-      (io/resource)
-      (slurp)))
+  (if-let [r (-> (str "input/" i) (io/resource))]
+    (slurp r)
+    (let [result (from-http i)]
+      (spit (resource-file i) result)
+      result)))
