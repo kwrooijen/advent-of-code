901a97eb9a46810831dc9f4e997494dc17786776
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2020-12-07 22:30:42 +0100

    Day 6

--- src/aoc_2020/day6.clj
+++ src/aoc_2020/day6.clj
@@ -4,3 +4,25 @@
    [clojure.string :as string]))
 
 (def data (get-input 2020 6))
+
+;; part one
+(->> (string/split data #"\n\n")
+     (mapv #(string/split % #"\n"))
+     (mapv (partial reduce into #{}))
+     (mapv count)
+     (apply +))
+
+;; part two
+
+(defn uniques [[h & rest]]
+  (->> h
+       (mapv (fn [x] [x (every? (partial some #{x}) rest)]))
+       (filter last)
+       (mapv first)
+       count))
+
+(->> (string/split data #"\n\n")
+     (mapv #(string/split % #"\n"))
+     (mapv (partial (partial mapv (comp set seq))))
+     (mapv uniques)
+     (apply +))
